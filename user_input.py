7bf74535c4faa08c7edf4f1fe800e63cc31a4c1f
denisewambui1 <160730133+denisewambui1@users.noreply.github.com>
2024-02-29 19:26:36 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+name = input("Enter name:" )
+age = ("Enter age:" )
+location = ("Enter location:" )
+
+print(" name is: " + name)
+print("age is: " + age)
+print("location is: " +location)
+
+message= ("Hello [name], you are [age] years old and live in [location]")
+print(message)
